df89509ac6e40971feeb12ca8031a110369447de
Spike837 <lda_fvg@alice.it>
2019-02-12 14:07:17 +0100

    test

--- lorenzo/test.js
+++ lorenzo/test.js
@@ -1,15 +1,26 @@
 var http = require('http');
-var server = http.createServer(function (req, res) {
+var sever = http.createServer(function (req, res) {
+    
+    var a = 3
+    var b= 3;
+
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hola ' + filename)
+  res.end('Hola ' + (+a+b))
   
 })
+console.log(server._events.connection);
+
+
+
 server.listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
-var path = require('path');
 
-var filename = path.basename('/home/tirocinio19/lorenzo/test.js', '.js');
+
+var filename 
+setTimeout(e=>{
+filename = path.basename('/home/tirocinio19/lorenzo/test.js', '.js')},5000000)
+ 
 console.log(filename);
 
 
